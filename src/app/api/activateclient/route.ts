115a6c68e661f6593b11d46534e30144aa648320
Antoni Jurga <104085107+rybijakKarpiowy@users.noreply.github.com>
2023-10-16 12:17:18 +0200

    Fix activation request notification

--- src/app/api/activateclient/route.ts
+++ src/app/api/activateclient/route.ts
@@ -100,24 +100,6 @@ export const POST = async (req: NextRequest) => {
             return NextResponse.json("User not found", { status: 500 });
         }
 
-        const { data: emailsToSendTo, error: error5 } = await pgsql.query.admin_emails
-            .findMany({
-                columns: {
-                    email: true,
-                },
-            })
-            .then((data) => ({ data, error: null }))
-            .catch((error) => ({ data: null, error }));
-
-        if (error5) {
-            console.log(error5);
-            return NextResponse.json(error5.message, { status: 500 });
-        }
-
-        if (!emailsToSendTo) {
-            return NextResponse.json("No emails to send to", { status: 500 });
-        }
-
         const msgtoClient = {
             to: activatedUserEmail.email,
             from: {
@@ -133,33 +115,12 @@ export const POST = async (req: NextRequest) => {
             html: eu === true ? activationEu : activationPl,
         };
 
-        const msgtoAdmin = {
-            to: emailsToSendTo.map((email) => email.email),
-            from: {
-                name: "Pro Media",
-                email: "biuro@kubki.com.pl",
-            },
-            subject: `${activatedUserEmail.company_name} czeka na aktywację`,
-            text: `${activatedUserEmail.company_name} czeka na aktywację`,
-            html: `<!DOCTYPE html><html><body><h1>${activatedUserEmail.company_name} czeka na aktywację</h1></body></html>`,
-        };
-
         sgmail.setApiKey(process.env.SENDGRID_KEY!);
         // send email to client
         await sgmail
             .send(msgtoClient)
             .then(() => {
                 console.log("Email to client sent");
-            })
-            .catch((error: any) => {
-                console.error(error);
-            });
-
-        // send email to admin/specified email
-        await sgmail
-            .send(msgtoAdmin)
-            .then(() => {
-                console.log("Email to admin sent");
             })
             .catch((error: any) => {
                 console.error(error);
--- src/app/api/createuser/route.ts
+++ src/app/api/createuser/route.ts
@@ -4,6 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { baseUrl } from "@/app/baseUrl";
 import { pgsql } from "@/database/pgsql";
 import * as schema from "@/database/schema";
+import sgmail from "@sendgrid/mail";
 
 export const GET = async (req: NextRequest) => {
     const res = NextResponse.next();
@@ -24,6 +25,66 @@ export const GET = async (req: NextRequest) => {
     if (error) {
         return NextResponse.json(error.message, { status: 500 });
     }
+
+    const { data: activatedUserEmail, error: error2 } = await pgsql.query.users
+        .findFirst({
+            where: (users, { eq }) => eq(users.user_id, auth_id),
+            columns: {
+                email: true,
+                eu: true,
+                company_name: true,
+            },
+        })
+        .then((data) => ({ data, error: null }))
+        .catch((error) => ({ data: null, error }));
+
+    if (error2) {
+        return NextResponse.json(error2.message, { status: 500 });
+    }
+
+    if (!activatedUserEmail) {
+        return NextResponse.json("User not found", { status: 500 });
+    }
+
+    const { data: emailsToSendTo, error: error3 } = await pgsql.query.admin_emails
+        .findMany({
+            columns: {
+                email: true,
+            },
+        })
+        .then((data) => ({ data, error: null }))
+        .catch((error) => ({ data: null, error }));
+
+    if (error3) {
+        console.log(error3);
+        return NextResponse.json(error3.message, { status: 500 });
+    }
+
+    if (!emailsToSendTo) {
+        return NextResponse.json("No emails to send to", { status: 500 });
+    }
+
+    const msgtoAdmin = {
+        to: emailsToSendTo.map((email) => email.email),
+        from: {
+            name: "Pro Media",
+            email: "biuro@kubki.com.pl",
+        },
+        subject: `${activatedUserEmail.company_name} czeka na aktywację`,
+        text: `${activatedUserEmail.company_name} czeka na aktywację`,
+        html: `<!DOCTYPE html><html><body><h1>${activatedUserEmail.company_name} czeka na aktywację</h1></body></html>`,
+    };
+
+    sgmail.setApiKey(process.env.SENDGRID_KEY!);
+    // send email to admin/specified email
+    await sgmail
+        .send(msgtoAdmin)
+        .then(() => {
+            console.log("Email to admin sent");
+        })
+        .catch((error: any) => {
+            console.error(error);
+        });
 
     return NextResponse.json({ status: 201 });
 };

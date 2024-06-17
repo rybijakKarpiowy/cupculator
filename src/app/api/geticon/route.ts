f7b5ac004cb5831ac0a8f4fb99f82ef62c65c010
Antoni Jurga <antekjurga@gmail.com>
2024-06-17 12:20:29 +0200

    Icon fix

--- src/app/api/geticon/route.ts
+++ src/app/api/geticon/route.ts
@@ -1,14 +1,24 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export const POST = async (req: NextRequest) => {
-    const { icon } = await req.json();
+    const { icon } = (await req.json()) as { icon: string }
 
-    const iconImg = await fetch(icon).then((res) => res.blob());
+    let res = await fetch(icon)
+
+    if (!res.ok) {
+        if (icon.slice(4, 5) != "s") {
+            return new NextResponse("error", {status: 500})
+        }
+        const iconHttp = icon.slice(0, 4) + icon.slice(5);
+        res = await fetch(iconHttp)
+    }
+
+    const iconImg = await res.blob()
 
     return new NextResponse(iconImg, {
         headers: {
             "Content-Type": "image/jpg",
-            "Cache-Control": "public, max-age=3600, immutable",
+            "Cache-Control": "public, max-age=360000, immutable",
         },
     });
 };

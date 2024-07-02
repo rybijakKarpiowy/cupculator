c5fba1995d6c2bd9353658f3f925ddde713b6f8c
Antoni Jurga <antekjurga@gmail.com>
2024-07-02 13:13:11 +0200

    Axios geticon

--- src/app/api/geticon/route.ts
+++ src/app/api/geticon/route.ts
@@ -1,37 +1,22 @@
+import axios from "axios";
 import { NextRequest, NextResponse } from "next/server";
-import https from 'https';
 
 export const POST = async (req: NextRequest) => {
     const { icon } = (await req.json()) as { icon: string }
 
-    const options = {
-        hostname: 'kubki.com.pl',
-        path: icon,
+    const response = await axios({
+        method: "get",
+        url: icon,
         headers: {
-            'Accept-Language': 'en-US,en;q=0.8',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
-            'Host': 'kubki.com.pl'
-        }
-    };
-
-    let promise = new Promise((resolve, reject) => {
-        const my_req = https.get(options, function (res) {
-            const chunks: Buffer[] = [];
-    
-            res.on('data', function (chunk) {
-                chunks.push(chunk);
-            });
-    
-            res.on('end', function () {
-                const body = Buffer.concat(chunks);
-                const blob = new Blob([body], {type: "image/jpeg"});
-                resolve(blob)
-            });
-        });
-        my_req.on("error", (e) => reject(e));
+            "Host": "kubki.com.pl",
+            "Cache-Control": "no-cache",
+            "Accept": "*/*"
+        },
+        responseType: "arraybuffer"
     })
 
-    const iconBlob = await promise as Blob
+    const arrayBuffer = response.data as ArrayBuffer
+    const iconBlob = new Blob([arrayBuffer])
     
     return new NextResponse(iconBlob, {
         headers: {

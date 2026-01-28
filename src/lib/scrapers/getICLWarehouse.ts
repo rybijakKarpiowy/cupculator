0c3f740efebe13f07110494511c37aa301cbbe47
Ashley Jurga <ashleyjurga@gmail.com>
2026-01-28 23:22:44 +0100

    Fix icl test

--- src/lib/scrapers/getICLWarehouse.ts
+++ src/lib/scrapers/getICLWarehouse.ts
@@ -85,7 +85,14 @@ export const getICLWarehouse = async (cups: { cup_id: number; link: string }[]) 
 
     const pageCups = cupPagesBody.map((page) => {
         const body = page.body;
-        const amount = parseInt(body(".product section div div div div div div div div span b").first().text().replace(" ", "").trim()) || 0;
+        const amount =
+			parseInt(
+				body(".product div div div span b")
+					.first()
+					.text()
+					.replace(" ", "")
+					.trim()
+			) || 0;
 
         return { code_link: page.link, amount };
     });
